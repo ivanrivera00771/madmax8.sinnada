849aef4f3496e9e99a8e702c7353522426cf647a
IvanRivera771 <47260927+IvanRivera771@users.noreply.github.com>
2020-12-05 18:14:37 -0600

    PRIMER APORTE ALV

---
+++ src/madmax8/sinnada/Madmax8Sinnada.java
@@ -0,0 +1,12 @@
+
+package madmax8.sinnada;
+
+
+public class Madmax8Sinnada {
+
+ 
+    public static void main(String[] args) {
+        System.out.println("MADMAX8.SIN NI MIERDA");
+    }
+    
+}
