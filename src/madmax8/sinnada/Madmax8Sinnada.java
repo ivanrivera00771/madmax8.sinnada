6528a96c429b33e6d708a56a586e1631236c2ec7
ivanrivera00771 <75515090+ivanrivera00771@users.noreply.github.com>
2020-12-07 10:03:48 -0600

    Update Madmax8Sinnada.java
    COLOQUE UNA ACTUALIZACION

--- src/madmax8/sinnada/Madmax8Sinnada.java
+++ src/madmax8/sinnada/Madmax8Sinnada.java
@@ -7,6 +7,8 @@ public class Madmax8Sinnada {
  
     public static void main(String[] args) {
         System.out.println("MADMAX8.SIN NI MIERDA");
+        System.out.println("MADMAX9.SIN NI MIERDA 2");
+      
     }
     
 }
